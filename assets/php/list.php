a1559c5973a74ca8ab441b90baccc2fd667bdab4
DzenetanMassart <dzenetan.massart@gmail.com>
2019-04-15 16:39:43 +0200

    Le visuel est plus intéressant !

--- assets/php/list.php
+++ assets/php/list.php
@@ -6,7 +6,9 @@ error_reporting(E_ALL);
 /* cette page sert à gérer la liste des tâches en cours */
 include 'connect.php';
 
-if(isset($_POST['conserve']))
+if(isset($_POST['conserve'])){
+if(isset($_POST['UNDONE']))
+
 {$pas_fait=$_POST['UNDONE'];
 
     foreach($pas_fait as $inc){
@@ -16,8 +18,10 @@ if(isset($_POST['conserve']))
     $bdd->exec($values);
    };
 };
+};
     if(isset($_POST['conserve_tout']))
 {$bdd->exec("UPDATE taches SET statut=1");}
+
 header('Location: ../../index.php');
 
 ?>
--- index.php
+++ index.php
@@ -12,12 +12,13 @@ require 'assets/php/connect.php';
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-<link rel="stylesheet" href="assets/css/todolist.css">
+<link rel="stylesheet" href="assets/css/todolist.min.css">
+
 </head>
 <body>
 
 <header>
-		<h1>ToDoList SQL</h1>
+		<h1><i class="fas fa-clipboard-list"></i>ToDoList SQL</h1>
 	</header>
 
 	<section>
@@ -41,10 +42,10 @@ require 'assets/php/connect.php';
 				}
 				
 				?>
-
+<div class="separe">
 				<input type="submit" value="Archiver" name="conserve">
 				<input type="submit" value="Archiver TOUT" name="conserve_tout">
-
+			</div>
 			</form>
 		</div>
 
@@ -69,16 +70,20 @@ require 'assets/php/connect.php';
 				}
 				
 				?>
+<div class="separe">
 
 				<input  type="submit" value="Supprimer" name="retire">
 				<input  type="submit" value="Supprimer TOUT" name="retire_tout">
-
+			</div>
 			</form>
 		</div>
 
-		<form action="assets/php/update.php" method="POST">			
-            <textarea placeholder="Ajouter une tache" name="plusTache" class="tache" rows="1" cols="33"></textarea>
-            <input  type="submit" value="Enregistrer" class="submit" name="enregistrer">
+		<form action="assets/php/update.php" method="POST">	
+		<div class="separe">
+		
+            <input type="text" placeholder="Ajouter une tache" name="plusTache" class="tache" rows="1" cols="33">
+			<input  type="submit" value="Enregistrer" class="submit" name="enregistrer">
+			</div>
 		</form>
 
 	</section>
